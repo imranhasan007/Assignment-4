8f05d7a25b8206b153ce9b38512c341ece6d6921
imranhasan007 <imranhasanansari0786@gmail.com>
2025-11-09 02:14:34 +0530

    Initial Commit

---
+++ Using the Math Module for Calculations.py
@@ -0,0 +1,11 @@
+import math
+number = float(input("Enter a number: "))
+
+sqrt_val = math.sqrt(number)
+print(f"Square root: {sqrt_val}")
+
+log_val = math.log(number)
+print(f"Logarithm: {log_val}")
+
+sine_val = math.sin(number)
+print(f"Sine: {sine_val}")
